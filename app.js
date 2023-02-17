24ed44ac08aaca5a4e374c07aa3fab419c9e8377
Aakash Chaudhary <akashchaudhary12121@gmail.com>
2023-02-17 12:31:04 +0530

    CORS added for API integration

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
 const bodyParser = require("body-parser")
 app.use(bodyParser.json())
+
+const allowedOrigins = ['http://localhost:3000'];
+
+app.use(cors({
+    origin(origin, callback) {
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        // if (!origin) return callback(null, true);
+        // if (allowedOrigins.indexOf(origin) === -1) {
+        //     const msg = 'The CORS policy for this site does not ' +
+        //         'allow access from the specified Origin.';
+        //     return callback(new Error(msg), false);
+        // }
+        return callback(null, true);
+    }
+}));
 
 // Auth Routes
 const auth = require("./routes/auth.routes")
